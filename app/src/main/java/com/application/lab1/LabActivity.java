075766543a4cdd40aeed8668044bf929d066df97
amalik23 <amalik23@wisc.edu>
2021-09-26 13:23:44 -0500

    P2 Pt 1

--- app/src/main/java/com/application/lab1/LabActivity.java
+++ app/src/main/java/com/application/lab1/LabActivity.java
@@ -2,9 +2,31 @@ package com.application.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LabActivity extends AppCompatActivity {
+
+    public void clickFunc(View view) {
+        EditText myTextField = (EditText) findViewById(R.id.editTextTextPersonName);
+        String str = myTextField.getText().toString();
+
+        //Toast.makeText(LabActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+
+        goToActivity2(str);
+
+    }
+
+    public void goToActivity2(String s){
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("message",s);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
